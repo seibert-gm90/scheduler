18ba20f6f1c3624e7372be65af2a143d9f220bca
Gabby Jones <jones.gm90@gmail.com>
2019-12-11 22:26:18 -0500

    working on functions.

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -1,11 +1,33 @@
 moment().format();
 
 
-$ (document).ready(function(){
+$(document).ready(function () {
     var currentHour = moment().format("h a");
     var currentDate = moment().format("dddd, MMMM Do YYYY");
-//display date and time
-$("#todaysDate").text(currentDate)
-console.log(currentDate)
+    //display date and time
+    $("#todaysDate").text(currentDate)
+    //console.log(currentDate)
 })
 
+var ul = $("#timeSlots")
+//var li = $(".hourSlot")
+var saveButton = $(".save")
+
+
+function valueCheck() {
+
+    for (var i = 0; i < li.length; i++) {
+        $(".hourSlot").val();
+        console.log(valueCheck())
+    }
+
+}
+
+function buttonClick() {
+    preventDefault();
+    saveButton.click(function () {
+        localStorage.setItem('storeObj', JSON.stringify(myObj));
+
+
+    });
+}
