e852dcd86a9c053663070de23cfc3d1069b0817e
Gabby Jones <jones.gm90@gmail.com>
2019-12-13 12:00:12 -0500

    adding function for local storage and checking value of input.

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -1,6 +1,3 @@
-moment().format();
-
-
 $(document).ready(function () {
     var currentHour = moment().format("hh:mm:ss A");
     var currentDate = moment().format("dddd, MMMM Do YYYY");
@@ -8,40 +5,42 @@ $(document).ready(function () {
     $("#todaysDate").text(currentDate)
     $("#currentTime").text(currentHour)
     //console.log(currentDate)
-   
-})
+    //update page with current time as user is on site//
 
-function updateCurrentTime(){
-var updatedTime = setInterval(function() {
-    currentDate = moment().format("dddd, MMMM Do YYYY");
-    $("#todaysDate").text(currentDate);
-    currentHour = moment().format("hh:mm:ss A");
-    $("#currentTime").text(updatedTime);
-}, 1000);
-};
-updateCurrentTime()
+    function updateCurrentTime() {
+        var updatedTime = setInterval(function () {
+            var currentDate = moment().format("dddd, MMMM Do YYYY");
+            var currentHour = moment().format("hh:mm:ss A");
+            $("#todaysDate").text(currentDate);
+            $("#currentTime").text(currentHour);
+        }, 1000);
+    };
+    updateCurrentTime()
 
 
-var ul = $("#timeSlots")
-var li = $(".hourSlot")
-var saveButton = $(".save")
-var inputVal = $(".hourSlot").val();
-var valArray = [""]
+    var ul = $("#timeSlots")
+    var li = $(".hourSlot")
+    var saveButton = $(".save")
+    var inputVal = $(".userText").val();
+    var valArray = [""]
 
-function valueCheck() {
+    function valueCheck() {
 
-    for (var i = 0; i < ul.length; i++) {
-        $(".hourSlot").val();
-        var inputVal = $(".hourSlot").val();
-        valArray.push(inputVal)
+        for (var i = 0; i < ul.length; i++) {
+            $(".userText").val();
+            var inputVal = $(".userText").val();
+            valArray.push(inputVal)
+        }
+        valueCheck()
     }
 
-}
-
-function saveLocalStorage() {
-    saveButton.click(function () {
+    $(".save").click(function () {
         event.preventDefault();
-        $(".hourSlot").val();
-        localStorage.setItem('storeObj', JSON.stringify(myObj));
-        var user = JSON.parse(localStorage.getItem('user'));
-    })}; 
+        var index = $(this).attr("data-slot")
+        var input = $(".userText")[index]
+        var task = $(input).val() 
+        localStorage.setItem("valArray", task)
+        // $(localStorage.getItem('valArray', JSON.stringify(myObj));
+        // var user = JSON.parse(localStorage.getItem('user'));
+    })
+})
