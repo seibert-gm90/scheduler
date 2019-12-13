e82279bd4b8e1a0090c7cc189eeabd5b4f49acb6
Gabby Jones <jones.gm90@gmail.com>
2019-12-12 23:49:08 -0500

    working on functions to save to local storage

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -2,32 +2,46 @@ moment().format();
 
 
 $(document).ready(function () {
-    var currentHour = moment().format("h a");
+    var currentHour = moment().format("hh:mm:ss A");
     var currentDate = moment().format("dddd, MMMM Do YYYY");
     //display date and time
     $("#todaysDate").text(currentDate)
+    $("#currentTime").text(currentHour)
     //console.log(currentDate)
+   
 })
 
-var ul = $("#timeSlots")
-//var li = $(".hourSlot")
-var saveButton = $(".save")
+function updateCurrentTime(){
+var updatedTime = setInterval(function() {
+    currentDate = moment().format("dddd, MMMM Do YYYY");
+    $("#todaysDate").text(currentDate);
+    currentHour = moment().format("hh:mm:ss A");
+    $("#currentTime").text(updatedTime);
+}, 1000);
+};
+updateCurrentTime()
 
+
+var ul = $("#timeSlots")
+var li = $(".hourSlot")
+var saveButton = $(".save")
+var inputVal = $(".hourSlot").val();
+var valArray = [""]
 
 function valueCheck() {
 
-    for (var i = 0; i < li.length; i++) {
+    for (var i = 0; i < ul.length; i++) {
         $(".hourSlot").val();
-        console.log(valueCheck())
+        var inputVal = $(".hourSlot").val();
+        valArray.push(inputVal)
     }
 
 }
 
-function buttonClick() {
-    preventDefault();
+function saveLocalStorage() {
     saveButton.click(function () {
+        event.preventDefault();
+        $(".hourSlot").val();
         localStorage.setItem('storeObj', JSON.stringify(myObj));
-
-
-    });
-}
+        var user = JSON.parse(localStorage.getItem('user'));
+    })}; 
